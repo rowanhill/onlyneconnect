8c870fc229ef1c70af3d3699ca4ba5a4631f9b4f
Rowan Hill <rowan.hill@gmail.com>
2021-01-23 17:59:36 +0000

    Fix indentation

--- src/answerScoreCalculator.ts
+++ src/answerScoreCalculator.ts
@@ -4,11 +4,11 @@ export const calculateScore = (question: Question, clueIndex: number) => {
     switch (question.type) {
         case 'connection':
         case 'sequence':
-        if (clueIndex === 0) {
-            return 5;
-        } else {
-            return 4 - clueIndex;
-        }
+            if (clueIndex === 0) {
+                return 5;
+            } else {
+                return 4 - clueIndex;
+            }
         default:
             throwBadQuestionType(question);
     }
