8406e6a3995ec41812f2374f26c6fedda115284b
Rowan Hill <rowan.hill@gmail.com>
2021-02-28 12:37:43 +0000

    A little more styling on quiz create page

--- src/QuizCreatePage.tsx
+++ src/QuizCreatePage.tsx
@@ -40,11 +40,19 @@ const QuizCreatePage = () => {
 
     return (
         <Page title="Create a new quiz">
-            <Card>
+            <Card title="Quiz basics">
                 <form onSubmit={handleSubmit}>
                     <fieldset disabled={isSubmitting}>
-                        <input type="text" placeholder="Quiz name" onChange={handleNameChange} value={quizName} />
-                        <input type="text" placeholder="Passcode" onChange={handlePasscodeChange} value={passcode} />
+                        <div>
+                            <h4><label>Quiz name</label></h4>
+                            <input type="text" placeholder="Quiz name" onChange={handleNameChange} value={quizName} name="quiz-title" />
+                            <p>The quiz name is the title your quiz will have. All players will be able to see this name.</p>
+                        </div>
+                        <div>
+                            <h4><label>Passcode</label></h4>
+                            <input type="text" placeholder="Passcode" onChange={handlePasscodeChange} value={passcode} />
+                            <p>The passcode is a secret phrase people must enter to create a team.</p>
+                        </div>
                         <PrimaryButton>Create</PrimaryButton>
                     </fieldset>
                 </form>
