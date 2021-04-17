63dbc9355055615090abf32d2e7e3685f3751ada
Rowan Hill <rowan.hill@gmail.com>
2021-04-17 23:44:10 +0100

    Pause timers when question / quiz closed

--- src/QuestionTimer.tsx
+++ src/QuestionTimer.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { useCluesContext } from './contexts/quizPage';
 import { useAnimationTimer } from './hooks/useAnimationFrame';
 import { CollectionQueryItem } from './hooks/useCollectionResult';
 import { Question, Clue, getClueIds, Quiz } from './models';
@@ -20,38 +21,78 @@ function useStartTime<T>(resetKey: T, predicate?: (resetKey: T, hasBeenSet: bool
     return startTimeRef.current;
 }
 
+function useEndTime({ now, disable, pause }: { now: number; disable?: boolean; pause?: boolean; }) {
+    const endTimeRef = useRef<number | null>(null);
+
+    if (disable === true) {
+        endTimeRef.current = null;
+    } else if (pause !== true) {
+        endTimeRef.current = now;
+    }
+
+    return endTimeRef;
+}
+
+function useIsQuestionClosed(currentQuestionItem?: CollectionQueryItem<Question>) {
+    const { data: cluesData } = useCluesContext();
+    let isQuestionClosed = null;
+    if (currentQuestionItem && cluesData) {
+        const clueIds = getClueIds(currentQuestionItem.data);
+        const lastClueId = clueIds[clueIds.length - 1];
+        const lastClue = cluesData.find((c) => c.id === lastClueId);
+        if (lastClue) {
+            isQuestionClosed = lastClue.data.closedAt === undefined;
+        }
+    }
+    return isQuestionClosed;
+}
+
 interface QuestionTimerProps {
     currentQuestionItem?: CollectionQueryItem<Question>;
     currentClueItem?: CollectionQueryItem<Clue>;
     quiz: Quiz;
 }
 export const QuestionTimer = ({ currentQuestionItem, currentClueItem, quiz }: QuestionTimerProps) => {
+    const isQuestionClosed = useIsQuestionClosed(currentQuestionItem);
     const time = useAnimationTimer(1000);
     const quizStartTime = useStartTime(currentQuestionItem, (item, hasBeenSet) => !hasBeenSet && item !== undefined);
     const questionStartTime = useStartTime(currentQuestionItem?.id);
     const clueStartTime = useStartTime(currentClueItem?.id);
 
+    const quizEndTime = useEndTime({
+        now: time,
+        pause: quiz.isComplete,
+    });
+    const questionEndTime = useEndTime({
+        now: time,
+        disable: !currentQuestionItem || isQuestionClosed === null,
+        pause: isQuestionClosed === false,
+    });
+    const clueEndTime = useEndTime({
+        now: time,
+        disable: !currentClueItem,
+        pause: !!currentClueItem?.data.closedAt,
+    });
+
     if (currentQuestionItem === undefined) {
         return (null);
     }
 
-    const TimerBlock = ({ title, start }: { title: string; start: number; }) => {
-        return (
-            <div className={styles.timerBlock}>
-                <span className={styles.timerTitle}>{title}</span>
-                <TimeSince start={start} now={time} />
-            </div>
-        );
-    };
-    const QuizTimer = () => <TimerBlock title="Quiz" start={quizStartTime} />;
-    const QuestionTimer = () => <TimerBlock title="Question" start={questionStartTime} />;
-    const ClueTimer = () => <TimerBlock title="Clue" start={clueStartTime} />;
+    const quizTimer = () => quizEndTime.current ?
+        <TimerBlock title="Quiz" start={quizStartTime} time={quizEndTime.current} /> :
+        null;
+    const questionTimer = () => questionEndTime.current ?
+        <TimerBlock title="Question" start={questionStartTime} time={questionEndTime.current} /> :
+        null;
+    const clueTimer = () => clueEndTime.current ?
+        <TimerBlock title="Clue" start={clueStartTime} time={clueEndTime.current} /> :
+        null;
 
     if (currentClueItem === undefined || !currentQuestionItem.data.isRevealed) {
         return (
             <div className={styles.timers}>
-                <QuizTimer />
-                {!quiz.isComplete && <QuestionTimer />}
+                {quizTimer()}
+                {!quiz.isComplete && questionTimer()}
             </div>
         );
     } else {
@@ -59,33 +100,39 @@ export const QuestionTimer = ({ currentQuestionItem, currentClueItem, quiz }: Qu
         const isLastClue = clueIds[clueIds.length - 1] === currentClueItem.id;
         const lastClueIsClosed = isLastClue && currentClueItem.data.closedAt !== undefined;
         if (lastClueIsClosed) {
-            return (<div>Quiz: <TimeSince start={quizStartTime} now={time} /></div>);
+            return (
+                <div className={styles.timers}>
+                    {quizTimer()}
+                </div>
+            );
         } else {
             const showClueTimer = clueIds.length > 1;
             return (
                 <div className={styles.timers}>
-                    <QuizTimer />
-                    <QuestionTimer />
-                    {showClueTimer && <ClueTimer />}
+                    {quizTimer()}
+                    {questionTimer()}
+                    {showClueTimer && clueTimer()}
                 </div>
             );
         }
     }
 };
 
-const TimeSince = ({ start, now }: { start: number; now: number; }) => {
-    const timeSinceChange = now - start;
-    return <MinsSecs millis={timeSinceChange} />;
-};
+const TimerBlock = ({ title, start, time }: { title: string; start: number; time: number }) => {
+    const timeSinceChange = time - start;
 
-const MinsSecs = ({ millis }: { millis: number; }) => {
-    const roundedSeconds = Math.max(0, Math.round(millis / 1000));
+    const roundedSeconds = Math.max(0, Math.round(timeSinceChange / 1000));
 
     const hours = Math.floor(roundedSeconds / 3600)
     const mins = Math.floor(roundedSeconds / 60) % 60;
     const secs = roundedSeconds % 60;
 
-    return <span className={styles.time}>{hours > 0 && pad(hours) + ':'}{pad(mins)}:{pad(secs)}</span>;
+    return (
+        <div className={styles.timerBlock}>
+            <span className={styles.timerTitle}>{title}</span>
+            <span className={styles.time}>{hours > 0 && pad(hours) + ':'}{pad(mins)}:{pad(secs)}</span>
+        </div>
+    );
 };
 
 function getCurrentTime(): number {
