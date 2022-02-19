81f1bd0d14f300a9faa33fb5841d77678ffb76df
Rowan Hill <rowan.hill@gmail.com>
2022-02-19 09:21:14 +0000

    Player Zoom UI tweaks including play icon

--- src/components/zoom/ZoomCallRemote.tsx
+++ src/components/zoom/ZoomCallRemote.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from 'react';
-import { DangerButton, PrimaryButton } from '../../Button';
+import { ComponentProps, RefObject, useRef, useState } from 'react';
+import { DangerButton } from '../../Button';
 import { useQuizContext } from '../../contexts/quizPage';
 import { useDisconnectAfterTimeout } from './hooks/useDisconnectAfterTimeout';
 import { useInitialisedZoomClient } from './hooks/useInitialisedZoomClient';
@@ -8,6 +8,7 @@ import { videoDimensions } from './videoConfig';
 import { ZoomCallTimeoutModal } from './ZoomCallTimeoutModal';
 import { ZoomClient } from './zoomTypes';
 import styles from './ZoomCallRemote.module.css';
+import { ReactComponent as PlayIcon } from './play.svg';
 
 export const ZoomCallRemote = () => {
     const zoomClient = useInitialisedZoomClient();
@@ -15,24 +16,8 @@ export const ZoomCallRemote = () => {
     if (zoomClient) {
         return <ZoomCallRemoteInitialised zoomClient={zoomClient} />;
     } else {
-        return <ZoomCallRemoteUninitialised />;
+        return <ZoomCallRemotePresentation state="zoom-loading" />;
     }
-};
-
-const ZoomCallRemoteUninitialised = () => {
-    return (
-        <>
-        <canvas
-            {...videoDimensions}
-            className={styles.hostVideoCanvas}
-            style={{ visibility: 'hidden' }}
-        />
-        <div>Disconnected</div>
-        <div>
-            <button disabled={true}>Loading...</button>
-        </div>
-        </>
-    );
 };
 
 const ZoomCallRemoteInitialised = ({ zoomClient }: { zoomClient: ZoomClient }) => {
@@ -46,34 +31,73 @@ const ZoomCallRemoteInitialised = ({ zoomClient }: { zoomClient: ZoomClient }) =
         () => setShouldBeInCall(false),
     );
 
-    const disconnect = () => {
-        zoomClient.leave();
+    const joinCall = () => {
+        setShouldBeInCall(true);
+    };
+    const leaveCall = () => {
         setShouldBeInCall(false);
     };
+    const disconnect = () => {
+        zoomClient.leave();
+        leaveCall();
+    };
 
-    const status = shouldBeInCall ?
-        (quiz.isZoomSessionLive ?
-            (videoHasStarted ?
-                'Joined' :
-                'Connecting'
-            ) :
-            'Waiting for host'
-        ) :
-        'Disconnected';
+    let state: ComponentProps<typeof ZoomCallRemotePresentation>['state'];
+    if (shouldBeInCall) {
+        if (videoHasStarted) {
+            state = 'joined';
+        } else {
+            if (quiz.isZoomSessionLive) {
+                state = 'connecting';
+            } else {
+                state = 'lobby';
+            }
+        }
+    } else {
+        state = 'disconnected';
+    }
+
     return (
         <>
-        <canvas
-            ref={videoCanvasRef}
-            {...videoDimensions}
-            className={styles.hostVideoCanvas}
-            style={{ visibility: videoHasStarted ? 'initial' : 'hidden' }}
-        />
-        <div>{status}</div>
-        <div>
-            {shouldBeInCall && <DangerButton onClick={() => setShouldBeInCall(false)}>Leave call</DangerButton>}
-            {!shouldBeInCall && <PrimaryButton onClick={() => setShouldBeInCall(true)}>Join call</PrimaryButton>}
-        </div>
+        <ZoomCallRemotePresentation state={state} videoCanvasRef={videoCanvasRef} joinCall={joinCall} leaveCall={leaveCall} />
         {showTimeoutModal && <ZoomCallTimeoutModal onStayConnected={postponeTimeoutThirtyMinutes} onDisconnect={disconnect} />}
         </>
+    );
+};
+
+const ZoomCallRemotePresentation = (props: {
+    state: 'zoom-loading' | 'disconnected' | 'connecting' | 'lobby' | 'joined';
+    videoCanvasRef?: RefObject<HTMLCanvasElement>;
+    leaveCall?: () => void;
+    joinCall?: () => void;
+}) => {
+    return (
+        <div className={styles.playerWrapper}>
+            <div className={styles.videoWrapper}>
+                <canvas
+                    ref={props.videoCanvasRef}
+                    {...videoDimensions}
+                    className={styles.hostVideoCanvas}
+                    style={{ visibility: props.state === 'joined' ? 'initial' : 'hidden' }}
+                />
+                {(props.state === 'connecting' || props.state === 'lobby') &&
+                    <div className={styles.videoCover}>
+                        {props.state === 'connecting' ? 'Connecting' : 'Waiting for host'}
+                    </div>
+                }
+            </div>
+            <div className={styles.toolbar}>
+                <DangerButton
+                    style={{ visibility: (props.state === 'joined' || props.state === 'lobby') ? 'initial' : 'hidden'}}
+                    onClick={props.leaveCall}
+                >Leave call</DangerButton>
+            </div>
+            {(props.state === 'zoom-loading' || props.state === 'disconnected') &&
+                <div className={styles.playerCover}>
+                    {props.state === 'zoom-loading' && <>Loading...</>}
+                    {props.state ==='disconnected' && <PlayIcon className={styles.playSvg} onClick={props.joinCall} />}
+                </div>
+            }
+        </div>
     );
 };
