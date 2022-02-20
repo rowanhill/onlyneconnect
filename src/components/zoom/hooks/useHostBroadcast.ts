84793ef1ebf36dbef7505b3345d9671b80924348
Rowan Hill <rowan.hill@gmail.com>
2022-02-20 10:03:38 +0000

    Host endCall doesn't leave non-existant sessions

--- src/components/zoom/hooks/useHostBroadcast.ts
+++ src/components/zoom/hooks/useHostBroadcast.ts
@@ -1,11 +1,17 @@
 import { useQuizContext } from '../../../contexts/quizPage';
 import { ZoomClient } from '../zoomTypes';
-import { RefObject, useCallback, useEffect } from 'react';
+import { RefObject, useCallback, useEffect, useRef } from 'react';
 import { joinZoomSession } from '../zoomSession';
 import { updateZoomSessionStatus } from '../../../models/quiz';
 
 export const useHostBroadcast = (zoomClient: ZoomClient, videoRef: RefObject<HTMLVideoElement|null|undefined>) => {
     const { quiz, quizId } = useQuizContext();
+
+    // Keep a ref to whether the zoom session is marked as live in the DB, for use by callbacks
+    const zoomSessionLiveRef = useRef(quiz.isZoomSessionLive);
+    useEffect(() => {
+        zoomSessionLiveRef.current = quiz.isZoomSessionLive;
+    }, [quiz]);
 
     async function joinCall(didStartSession: { flag: boolean; }) {
         // Join the call - first user to join becomes host
@@ -24,10 +30,14 @@ export const useHostBroadcast = (zoomClient: ZoomClient, videoRef: RefObject<HTM
         // Mark the quiz's zoom session as being closed and wait for confirmation before proceeding
         // By waiting we reduce the window in which a player can request a zoom token and accidentally
         // become host.
-        await updateZoomSessionStatus(quizId, false);
+        if (zoomSessionLiveRef.current) {
+            await updateZoomSessionStatus(quizId, false);
+        }
 
         // Leave the call (and end it for all other participants)
-        zoomClient.leave(true);
+        if (zoomClient.getSessionInfo().isInMeeting) {
+            zoomClient.leave(true);
+        }
     }, [zoomClient, quizId]);
 
 
